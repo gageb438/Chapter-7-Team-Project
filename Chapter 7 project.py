df0c8a7c7afe9fa7e982edfe9cc392eff498b551
OwenSchroeder <owenschr14@gmail.com>
2025-01-16 13:41:31 -0600

    Create Chapter 7 project.py

---
+++ Chapter 7 project.py
@@ -0,0 +1,34 @@
+import random as r
+
+def main():
+    pass
+
+def output_dice(dice):
+    pass
+
+
+def roll_die():
+    #accepts no arguements
+    # generates and returns a random integers between 1-6
+    
+    dice = r.randint(1,6)
+    
+    return dice
+
+def first_roll():
+    pass
+
+def count_frequency(dice, number):
+    pass
+
+def find_mode(dice):
+    pass
+
+def list_unmatched_dice(dice):
+    pass
+
+def reroll_one(dice, index):
+    pass
+
+def reroll_many(dice):
+    pass
